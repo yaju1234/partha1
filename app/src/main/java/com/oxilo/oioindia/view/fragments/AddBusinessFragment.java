7659f2c8df4cbfe532e8e8ee3e9aa5163fd5ffc7
Your Name <you@example.com>
2018-02-27 16:57:44 +0530

    aa

--- app/src/main/java/com/oxilo/oioindia/view/fragments/AddBusinessFragment.java
+++ app/src/main/java/com/oxilo/oioindia/view/fragments/AddBusinessFragment.java
@@ -1,10 +1,17 @@
 package com.oxilo.oioindia.view.fragments;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.oxilo.oioindia.R;
 
@@ -12,7 +19,7 @@ import com.oxilo.oioindia.R;
  * Created by kamal on 02/15/2018.
  */
 
-public class AddBusinessFragment extends Fragment implements View.OnClickListener {
+public class AddBusinessFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     // TODO: Rename parameter arguments, choose names that match
 //    private static final String ARG_PARAM1 = "param1";
 //    private static final String ARG_PARAM2 = "param2";
@@ -32,6 +39,80 @@ public class AddBusinessFragment extends Fragment implements View.OnClickListene
 //    private CardView cardPrivacyPolicy;
 //    private CardView cardShare;
 
+    private Spinner spin_d11;
+    private Spinner spin_d12;
+    private Spinner spin_d21;
+    private Spinner spin_d22;
+    private Spinner spin_d31;
+    private Spinner spin_d32;
+    private Spinner spin_d41;
+    private Spinner spin_d42;
+    private Spinner spin_d51;
+    private Spinner spin_d52;
+    private Spinner spin_d61;
+    private Spinner spin_d62;
+    private Spinner spin_d71;
+    private Spinner spin_d72;
+    private String m_spin_d11 = "";
+    private String m_spin_d12 = "";
+    private String m_spin_d21 = "";
+    private String m_spin_d22 = "";
+    private String m_spin_d31 = "";
+    private String m_spin_d32 = "";
+    private String m_spin_d41 = "";
+    private String m_spin_d42 = "";
+    private String m_spin_d51 = "";
+    private String m_spin_d52 = "";
+    private String m_spin_d61 = "";
+    private String m_spin_d62 = "";
+    private String m_spin_d71 = "";
+    private String m_spin_d72 = "";
+    private String[] sSl = {"OPEN", "CLOSE"};
+    private ArrayAdapter sLAdapter;
+
+    private EditText et_company_name;
+    private EditText et_city;
+    private EditText et_area;
+    private EditText et_address_1;
+    private EditText et_address_2;
+    private EditText et_zip_code;
+    private EditText et_landmark;
+    private EditText et_description;
+    private EditText et_phone_1;
+    private EditText et_phone_2;
+    private EditText et_landline_number;
+    private EditText et_conatct_person;
+    private EditText et_website;
+    private EditText et_e_mail;
+    private EditText et_facebook;
+    private EditText et_twitter;
+    private EditText et_google_plus;
+
+    private ImageView iv_image;
+
+
+    private Button bt_submit;
+    private Button bt_choose_file;
+
+
+    private String m_et_company_name = "";
+    private String m_et_city = "";
+    private String m_et_area = "";
+    private String m_et_address_1 = "";
+    private String m_et_address_2 = "";
+    private String m_et_zip_code = "";
+    private String m_et_landmark = "";
+    private String m_et_description = "";
+    private String m_et_phone_1 = "";
+    private String m_et_phone_2 = "";
+    private String m_et_landline_number = "";
+    private String m_et_conatct_person = "";
+    private String m_et_website = "";
+    private String m_et_e_mail = "";
+    private String m_et_facebook = "";
+    private String m_et_twitter = "";
+    private String m_et_google_plus = "";
+
     public AddBusinessFragment() {
     }
 
@@ -40,8 +121,8 @@ public class AddBusinessFragment extends Fragment implements View.OnClickListene
     public static AddBusinessFragment newInstance() {
         AddBusinessFragment fragment = new AddBusinessFragment();
         Bundle args = new Bundle();
-       // args.putString(ARG_PARAM1, param1);
-       // args.putString(ARG_PARAM2, param2);
+        // args.putString(ARG_PARAM1, param1);
+        // args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -59,44 +140,157 @@ public class AddBusinessFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_my_add_business, null, false);
-        Toolbar toolbar=(Toolbar)v.findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-//        cardAboutUs=(CardView)v.findViewById(R.id.cardAboutUs);
-//        cardHelpSupport=(CardView)v.findViewById(R.id.cardHelpSupport);
-//        cardTramsCondition=(CardView)v.findViewById(R.id.cardTramsCondition);
-//        cardPrivacyPolicy=(CardView)v.findViewById(R.id.cardPrivacyPolicy);
-//        cardShare=(CardView)v.findViewById(R.id.cardShare);
-//        tvName=(TextView)v.findViewById(R.id.tvName);
-//        tvNumber=(TextView)v.findViewById(R.id.tvNumber);
-//        tvEmail=(TextView)v.findViewById(R.id.tvEmail);
-//        tvName.setText("Ajay Sodhi");
-//        tvNumber.setText("(9087678899");
-//        tvEmail.setText("test@gmail.com");
-//     AppController.getInstance().getAppPrefs().getObject("USER_ID",String.class);
-//        AppController.getInstance().getAppPrefs().getObject("FNAME", String.class);
-//        AppController.getInstance().getAppPrefs().getObject("LNAME", String.class);
-//        AppController.getInstance().getAppPrefs().getObject("EMAIL",String.class);
-//        AppController.getInstance().getAppPrefs().getObject("MOBILE", String.class);
 
-//        cardAboutUs.setOnClickListener(this);
-//        cardHelpSupport.setOnClickListener(this);
-//        cardTramsCondition.setOnClickListener(this);
-//        cardPrivacyPolicy.setOnClickListener(this);
-//        cardShare.setOnClickListener(this);
-        //toolbar
+        sLAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, sSl);
+        sLAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin_d11 = (Spinner) v.findViewById(R.id.spin_d11);
+        spin_d12 = (Spinner) v.findViewById(R.id.spin_d12);
+        spin_d21 = (Spinner) v.findViewById(R.id.spin_d21);
+        spin_d22 = (Spinner) v.findViewById(R.id.spin_d22);
+        spin_d31 = (Spinner) v.findViewById(R.id.spin_d31);
+        spin_d32 = (Spinner) v.findViewById(R.id.spin_d32);
+        spin_d41 = (Spinner) v.findViewById(R.id.spin_d41);
+        spin_d42 = (Spinner) v.findViewById(R.id.spin_d42);
+        spin_d51 = (Spinner) v.findViewById(R.id.spin_d51);
+        spin_d52 = (Spinner) v.findViewById(R.id.spin_d52);
+        spin_d61 = (Spinner) v.findViewById(R.id.spin_d61);
+        spin_d62 = (Spinner) v.findViewById(R.id.spin_d62);
+        spin_d71 = (Spinner) v.findViewById(R.id.spin_d71);
+        spin_d72 = (Spinner) v.findViewById(R.id.spin_d72);
+
+        et_company_name = (EditText) v.findViewById(R.id.et_company_name);
+        et_city = (EditText) v.findViewById(R.id.et_city);
+        et_area = (EditText) v.findViewById(R.id.et_area);
+        et_address_1 = (EditText) v.findViewById(R.id.et_address_1);
+        et_address_2 = (EditText) v.findViewById(R.id.et_address_2);
+        et_zip_code = (EditText) v.findViewById(R.id.et_zip_code);
+        et_landmark = (EditText) v.findViewById(R.id.et_landmark);
+        et_description = (EditText) v.findViewById(R.id.et_description);
+        et_phone_1 = (EditText) v.findViewById(R.id.et_phone_1);
+        et_phone_2 = (EditText) v.findViewById(R.id.et_phone_2);
+        et_landline_number = (EditText) v.findViewById(R.id.et_landline_number);
+        et_conatct_person = (EditText) v.findViewById(R.id.et_conatct_person);
+        et_website = (EditText) v.findViewById(R.id.et_website);
+        et_e_mail = (EditText) v.findViewById(R.id.et_e_mail);
+        et_facebook = (EditText) v.findViewById(R.id.et_facebook);
+        et_twitter = (EditText) v.findViewById(R.id.et_twitter);
+        et_google_plus = (EditText) v.findViewById(R.id.et_google_plus);
+
+        iv_image = (ImageView) v.findViewById(R.id.iv_image);
+
+        bt_submit = (Button) v.findViewById(R.id.bt_submit);
+        bt_choose_file = (Button) v.findViewById(R.id.bt_choose_file);
+
+
+        spin_d11.setAdapter(sLAdapter);
+        spin_d12.setAdapter(sLAdapter);
+        spin_d21.setAdapter(sLAdapter);
+        spin_d22.setAdapter(sLAdapter);
+        spin_d31.setAdapter(sLAdapter);
+        spin_d32.setAdapter(sLAdapter);
+        spin_d41.setAdapter(sLAdapter);
+        spin_d42.setAdapter(sLAdapter);
+        spin_d51.setAdapter(sLAdapter);
+        spin_d52.setAdapter(sLAdapter);
+        spin_d61.setAdapter(sLAdapter);
+        spin_d62.setAdapter(sLAdapter);
+        spin_d71.setAdapter(sLAdapter);
+        spin_d72.setAdapter(sLAdapter);
+
+        spin_d11.setOnItemSelectedListener(this);
+        spin_d12.setOnItemSelectedListener(this);
+        spin_d21.setOnItemSelectedListener(this);
+        spin_d22.setOnItemSelectedListener(this);
+        spin_d31.setOnItemSelectedListener(this);
+        spin_d32.setOnItemSelectedListener(this);
+        spin_d41.setOnItemSelectedListener(this);
+        spin_d42.setOnItemSelectedListener(this);
+        spin_d51.setOnItemSelectedListener(this);
+        spin_d52.setOnItemSelectedListener(this);
+        spin_d61.setOnItemSelectedListener(this);
+        spin_d62.setOnItemSelectedListener(this);
+        spin_d71.setOnItemSelectedListener(this);
+        spin_d72.setOnItemSelectedListener(this);
+
+        bt_submit.setOnClickListener(this);
+        bt_choose_file.setOnClickListener(this);
         return v;
 
     }
 
+    private void getValue() {
+        m_et_company_name = et_company_name.getText().toString().trim();
+        m_et_city = et_city.getText().toString().trim();
+        m_et_area = et_area.getText().toString().trim();
+        m_et_address_1 = et_address_1.getText().toString().trim();
+        m_et_address_2 = et_address_2.getText().toString().trim();
+        m_et_zip_code = et_zip_code.getText().toString().trim();
+        m_et_landmark = et_landmark.getText().toString().trim();
+        m_et_description = et_description.getText().toString().trim();
+        m_et_phone_1 = et_phone_1.getText().toString().trim();
+        m_et_phone_2 = et_phone_2.getText().toString().trim();
+        m_et_landline_number = et_landline_number.getText().toString().trim();
+        m_et_conatct_person = et_conatct_person.getText().toString().trim();
+        m_et_website = et_website.getText().toString().trim();
+        m_et_e_mail = et_e_mail.getText().toString().trim();
+        m_et_facebook = et_facebook.getText().toString().trim();
+        m_et_twitter = et_twitter.getText().toString().trim();
+        m_et_google_plus = et_google_plus.getText().toString().trim();
+
+        System.out.println("!!!m_et_company_name:="+m_et_company_name);
+        System.out.println("!!!m_et_city:="+m_et_city);
+        System.out.println("!!!m_et_area:="+m_et_area);
+        System.out.println("!!!m_et_address_1:="+m_et_address_1);
+        System.out.println("!!!m_et_address_2:="+m_et_address_2);
+        System.out.println("!!!m_et_zip_code:="+m_et_zip_code);
+        System.out.println("!!!m_et_landmark:="+m_et_landmark);
+        System.out.println("!!!m_et_description:="+m_et_description);
+        System.out.println("!!!m_et_phone_1:="+m_et_phone_1);
+        System.out.println("!!!m_et_phone_2:="+m_et_phone_2);
+        System.out.println("!!!m_et_landline_number:="+m_et_landline_number);
+        System.out.println("!!!m_et_conatct_person:="+m_et_conatct_person);
+        System.out.println("!!!m_et_website:="+m_et_website);
+        System.out.println("!!!m_et_e_mail:="+m_et_e_mail);
+        System.out.println("!!!m_et_facebook:="+m_et_facebook);
+        System.out.println("!!!m_et_twitter:="+m_et_twitter);
+        System.out.println("!!!m_et_google_plus:="+m_et_google_plus);
+
+
+        System.out.println("!!!m_spin_d11:="+m_spin_d11);
+        System.out.println("!!!m_spin_d12:="+m_spin_d12);
+        System.out.println("!!!m_spin_d21:="+m_spin_d21);
+        System.out.println("!!!m_spin_d22:="+m_spin_d22);
+        System.out.println("!!!m_spin_d31:="+m_spin_d31);
+        System.out.println("!!!m_spin_d32:="+m_spin_d32);
+        System.out.println("!!!m_spin_d41:="+m_spin_d41);
+        System.out.println("!!!m_spin_d42:="+m_spin_d42);
+        System.out.println("!!!m_spin_d51:="+m_spin_d51);
+        System.out.println("!!!m_spin_d52:="+m_spin_d52);
+        System.out.println("!!!m_spin_d61:="+m_spin_d61);
+        System.out.println("!!!m_spin_d62:="+m_spin_d62);
+        System.out.println("!!!m_spin_d71:="+m_spin_d71);
+        System.out.println("!!!m_spin_d72:="+m_spin_d72);
+
+
+    }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
+            case R.id.bt_submit:
+                getValue();
+                break;
+                case R.id.bt_choose_file:
+
+                break;
 //            case R.id.cardAboutUs:
 //                Intent in=new Intent(getActivity(), CommonActivity.class);
 //                in.putExtra("key","aboutUs");
@@ -127,6 +321,61 @@ public class AddBusinessFragment extends Fragment implements View.OnClickListene
 //                startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
 //                break;
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (view.getId()) {
+            case R.id.spin_d11:
+                m_spin_d11 = sSl[position];
+                break;
+            case R.id.spin_d12:
+                m_spin_d12 = sSl[position];
+                break;
+            case R.id.spin_d21:
+                m_spin_d21 = sSl[position];
+                break;
+            case R.id.spin_d22:
+                m_spin_d22 = sSl[position];
+                break;
+            case R.id.spin_d31:
+                m_spin_d31 = sSl[position];
+                break;
+            case R.id.spin_d32:
+                m_spin_d32 = sSl[position];
+                break;
+            case R.id.spin_d41:
+                m_spin_d41 = sSl[position];
+                break;
+            case R.id.spin_d42:
+                m_spin_d42 = sSl[position];
+                break;
+            case R.id.spin_d51:
+                m_spin_d51 = sSl[position];
+                break;
+            case R.id.spin_d52:
+                m_spin_d52 = sSl[position];
+                break;
+            case R.id.spin_d61:
+                m_spin_d61 = sSl[position];
+                break;
+            case R.id.spin_d62:
+                m_spin_d62 = sSl[position];
+                break;
+            case R.id.spin_d71:
+                m_spin_d71 = sSl[position];
+                break;
+            case R.id.spin_d72:
+                m_spin_d72 = sSl[position];
+                break;
+
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
